352ca52394ccf616106dec46875808cab0b0d607
Alexander Daniel <110725461+SolidCode321@users.noreply.github.com>
2026-01-27 17:02:32 +0300

    comment out

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 import base64
-import requests
+import httpx
 
 app = FastAPI()
 
@@ -10,12 +10,11 @@ LLAVA_URL = "http://83.96.121.186/api/chat"
 async def health_check():
     return {"status": "ok"}
 
-'''@app.post("/image")
+@app.post("/image")
 async def receive_image(request: Request):
-    print("recieving image")
-    body = await request.body()
+    print("receiving image")
 
-    # If Watcher sends raw JPEG
+    body = await request.body()
     img_b64 = base64.b64encode(body).decode()
 
     payload = {
@@ -30,10 +29,11 @@ async def receive_image(request: Request):
         "stream": False
     }
 
-    r = requests.post(LLAVA_URL, json=payload)
+    async with httpx.AsyncClient(timeout=60) as client:
+        r = await client.post(LLAVA_URL, json=payload)
+
     return r.json()
-'''
+
 @app.get("/image")
 async def image_check():
     return {"status": "ok"}
-
