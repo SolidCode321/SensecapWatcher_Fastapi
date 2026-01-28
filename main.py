d562cd5751a559612708c6494514808c8842fc77
Alexander Daniel <110725461+SolidCode321@users.noreply.github.com>
2026-01-28 10:44:49 +0300

    render

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def set_prompt(request: Request):
 
 
 # ---------- SenseCAP webhook ----------
-@app.post("/image")
+@app.post("/image/v1/notification/event")
 async def receive_image(request: Request):
     try:
         payload = await request.json()
